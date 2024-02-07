ad023b988c64f42ca2b13ca3ffe2434ca29b3664
oneminch <27751688+oneminch@users.noreply.github.com>
2024-02-07 17:18:21 -0500

    fix: refactor NextUI setup to fix routing issue with <Link>

--- frontend/src/pages/Root.tsx
+++ frontend/src/pages/Root.tsx
@@ -1,14 +1,21 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Header from "@/components/partials/Header";
+import Footer from "@/components/partials/Footer";
+import { NextUIProvider } from "@nextui-org/react";
 
 const Root = () => {
+  const navigate = useNavigate();
+
   return (
-    <>
-      <Header />
-      <main>
-        <Outlet />
-      </main>
-    </>
+    <NextUIProvider navigate={navigate}>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="grow">
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </NextUIProvider>
   );
 };
 
