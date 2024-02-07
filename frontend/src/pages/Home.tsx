85980502c2c7f92e892b2aef753af06f592749a1
oneminch <27751688+oneminch@users.noreply.github.com>
2024-02-07 17:21:38 -0500

    feat: create default layout for home page with game list from local data

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,31 @@
+import GameTitleCard from "@/components/GameTitleCard";
+import Filter from "@/components/partials/Filter";
 import Hero from "@/components/partials/Hero";
+import games from "@/data";
 
 const Home = () => {
   return (
     <>
-      <Hero />
+      <Hero
+        className="bg-white border border-gray-200 mt-4 mb-2 pt-10 pb-6"
+        textLabel="Play Your Next Favorite Story."
+        actionLink={{ label: "Browse", url: "#browse" }}
+      />
+      <div
+        className="flex flex-col md:flex-row items-start gap-y-4 md:gap-y-0 md:gap-x-4 py-4"
+        id="browse"
+      >
+        <Filter />
+        <article className="w-full min-h-96 rounded-lg">
+          <ul className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4">
+            {games.map((game) => (
+              <li className="mx-auto mb-2" key={game.title}>
+                <GameTitleCard game={game} />
+              </li>
+            ))}
+          </ul>
+        </article>
+      </div>
     </>
   );
 };
