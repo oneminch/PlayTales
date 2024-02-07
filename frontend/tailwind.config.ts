dc4457d5910736fb9dab22dd28d7a529ecf1571d
oneminch <27751688+oneminch@users.noreply.github.com>
2024-02-07 17:09:48 -0500

    refactor: customize default NextUI theme

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,3 +1,4 @@
+import defaultTheme from "tailwindcss/defaultTheme";
 import { nextui } from "@nextui-org/react";
 
 /** @type {import('tailwindcss').Config} */
@@ -7,9 +8,41 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
+  mode: "jit",
+  // important: "#root",
+  // corePlugins: {
+  //   preflight: false
+  // },
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        mono: ["'IBM Plex Mono'", ...defaultTheme.fontFamily.mono]
+      }
+    },
+    screens: {
+      xs: "475px",
+      ...defaultTheme.screens
+    }
   },
   darkMode: "class",
-  plugins: [nextui()]
+  plugins: [
+    nextui({
+      themes: {
+        dark: {
+          colors: { focus: "#fbbf24" }
+        },
+        light: {
+          colors: { focus: "#fbbf24" }
+        }
+      },
+      layout: {
+        radius: {
+          small: "0.25rem",
+          medium: "0.5rem",
+          large: "0.75rem"
+        }
+      }
+    })
+  ]
 };
