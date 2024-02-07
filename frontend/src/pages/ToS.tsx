0097f4b54b4cf580bdb10ca027b247875cea2733
oneminch <27751688+oneminch@users.noreply.github.com>
2024-02-07 17:15:14 -0500

    feat: create boilerplate Terms of Service page

---
+++ frontend/src/pages/ToS.tsx
@@ -0,0 +1,15 @@
+const ToS = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold">Terms of Service</h1>
+      <p>
+        By signing up or creating an account, you understand that PlayTales is
+        not a real service and therefore shouldn't even have a Terms of Service
+        page. Your account will be permanently purged from the database after a
+        certain period of time.
+      </p>
+    </>
+  );
+};
+
+export default ToS;
