e284e021ec95feb82404e59fedb740ab84596e5f
oneminch <27751688+oneminch@users.noreply.github.com>
2024-02-07 17:26:12 -0500

    feat: setup basic client-side filtering using search params

--- frontend/src/components/partials/Filter.tsx
+++ frontend/src/components/partials/Filter.tsx
@@ -1,21 +1,37 @@
-import { Link } from "react-router-dom";
+import { Button } from "@nextui-org/react";
+import { useSearchParams } from "react-router-dom";
+
+const FILTERS = [
+  "Action",
+  "Adventure",
+  "Indie",
+  "Puzzle",
+  "RPG",
+  "Shooter",
+  "Sports",
+  "Strategy"
+];
 
 const Filter = () => {
+  const [, setFilter] = useSearchParams();
+
+  const handleSetFilter = (filter: string) => {
+    setFilter({ category: filter });
+  };
+
   return (
-    <section className="flex items-center">
-      <h2 className="text-xl font-bold">Filter</h2>
-      <ul className="flex items-center justify-between space-x-2">
-        <li>
-          <Link to="/wishlist">Wishlist</Link>
-        </li>
-        <li>
-          <Link to="/cart">Cart</Link>
-        </li>
-        <li>
-          <Link to="/account">Account</Link>
-        </li>
+    <aside className="flex flex-row md:flex-col items-center md:items-start justify-between md:justify-start py-4 px-6 md:space-y-4 w-full md:w-1/5 shrink-0 bg-white border border-gray-200 rounded-lg">
+      <h2 className="text-xl font-bold text-gray-700 mr-4">Genres</h2>
+      <ul className="w-1/2 flex flex-row md:flex-col items-start justify-between md:justify-start md:space-y-2">
+        {FILTERS.map((filter) => (
+          <li key={filter}>
+            <Button onClick={() => handleSetFilter(filter.toLowerCase())}>
+              {filter}
+            </Button>
+          </li>
+        ))}
       </ul>
-    </section>
+    </aside>
   );
 };
 
