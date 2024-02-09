3487a44cc4665b781201798427f5122d91c17e11
oneminch <27751688+oneminch@users.noreply.github.com>
2024-02-08 20:27:09 -0500

    refactor: switch to using local JSON data file to select random images for every hero component

--- frontend/src/components/partials/Hero.tsx
+++ frontend/src/components/partials/Hero.tsx
@@ -1,8 +1,6 @@
-import { useState } from "react";
-import { Link } from "@nextui-org/react";
-import games from "@/data";
-
-const ALL_POSTERS = games.map((game) => game.posterImage);
+import { useEffect, useState } from "react";
+import { Image, Link } from "@nextui-org/react";
+import { GameTitle } from "@/types";
 
 const getMultipleRandom = (arr: string[], num: number) => {
   const shuffled = [...arr].sort(() => 0.5 - Math.random());
@@ -19,44 +17,41 @@ const Hero = ({
   textLabel: string;
   actionLink: Record<string, string>;
 }) => {
-  const [posters] = useState(() => getMultipleRandom(ALL_POSTERS, 3));
+  const [posters, setPosters] = useState<string[]>([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await fetch("/data.json");
+      const jsonData = await data.json();
+
+      const allPosters = jsonData.map((game: GameTitle) => game.posterImage);
+      setPosters(getMultipleRandom(allPosters, 3));
+    };
+    getData();
+  }, []);
 
   return (
     <section
-      className={`flex flex-col items-center justify-center rounded-lg space-y-6 ${className}`}
+      className={`flex flex-col items-center justify-center rounded-lg space-y-8 w-full ${className}`}
     >
       <div className="relative w-20 h-28 sm:w-24 sm:h-32">
-        <div
-          className={`absolute top-2 bottom-0 left-0 right-0 rounded-md origin-bottom rotate-12 translate-x-12 shadow-md ring-1 ring-gray-200/50`}
-          style={{
-            background: `url(${posters[0]})`,
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover"
-          }}
-        ></div>
-        <div
-          className={`absolute top-2 bottom-0 left-0 right-0 rounded-md origin-bottom -rotate-12 -translate-x-12 shadow-md ring-1 ring-gray-200/50`}
-          style={{
-            background: `url(${posters[1]})`,
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover"
-          }}
-        ></div>
-        <div
-          className={`absolute inset-0 rounded-md -translate-y-4 bg-white origin-bottom shadow-md`}
-          style={{
-            background: `url(${posters[2]})`,
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover"
-          }}
-        ></div>
+        {posters.map((poster) => (
+          <Image
+            key={poster}
+            width={96}
+            height={128}
+            removeWrapper
+            className="absolute top-2 bottom-0 left-0 right-0 rounded-md origin-bottom shadow-md ring-1 ring-gray-200/50 bg-gray-200 first:rotate-12 first:translate-x-12 even:-rotate-12 even:-translate-x-12 last:inset-0 last:scale-110 last:ring-0 last:-translate-y-0"
+            src={poster}
+          />
+        ))}
       </div>
       <h2 className="text-lg sm:text-xl font-semibold text-center text-gray-600">
         {textLabel}
       </h2>
       <Link
         href={actionLink.url}
-        className="px-4 py-1.5 rounded-xl font-medium bg-gray-900 text-gray-50 focus:global-focus"
+        className="px-4 py-1.5 rounded-lg font-medium bg-gray-900 text-gray-50 focus:global-focus"
       >
         {actionLink.label}
       </Link>
