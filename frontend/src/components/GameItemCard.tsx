efef574a54002888666eb8393ae8f7fdcc1cc0ea
oneminch <27751688+oneminch@users.noreply.github.com>
2024-02-08 20:21:30 -0500

    refactor: rename game item card

---
+++ frontend/src/components/GameItemCard.tsx
@@ -0,0 +1,33 @@
+import { Icon } from "@iconify/react";
+import { Button, Image } from "@nextui-org/react";
+import { Link } from "@nextui-org/react";
+import type { GameTitle } from "@/types";
+import GamePriceText from "./GamePriceText";
+
+const GameTitleCard = ({ game }: { game: GameTitle }) => {
+  return (
+    <Link
+      href={`/games/${game.id}`}
+      className="focus:global-focus relative p-1 pb-2 rounded-xl flex flex-col gap-y-1 items-start group no-underline"
+    >
+      <Image
+        className="w-full rounded-lg shadow opacity-100"
+        alt={`Poster Image for ${game.title}`}
+        src={game.posterImage}
+        removeWrapper={true}
+      />
+      <p className="font-bold text-sm text-left">{game.title}</p>
+      <GamePriceText price={game.price} discount={game.discount} />
+      <Button
+        isIconOnly
+        className="focus:global-focus flex items-center justify-center text-lg absolute top-3 right-3 bg-white border border-gray-200 rounded-full w-8 h-8 invisible scale-85 opacity-0 group-hover:visible group-hover:scale-100 group-hover:opacity-100 transition duration-150 z-20"
+        aria-label="Add to Wishlist"
+        size="sm"
+      >
+        <Icon icon="heroicons:heart" />
+      </Button>
+    </Link>
+  );
+};
+
+export default GameTitleCard;
