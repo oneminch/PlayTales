e8e0c5c868e3bb433274525b7a633c646a106f07
oneminch <27751688+oneminch@users.noreply.github.com>
2024-02-08 20:14:43 -0500

    refactor: switch use new local JSON data file and custom filtering component

--- frontend/src/pages/Wishlist.tsx
+++ frontend/src/pages/Wishlist.tsx
@@ -1,31 +1,36 @@
-import { Select, SelectItem } from "@nextui-org/react";
-import GameTitleCard from "@/components/GameTitleCard";
-import games from "@/data";
-
-const SORT_OPTIONS = [
-  { label: "Title - Asc", value: "title-asc" },
-  { label: "Title - Desc", value: "title-desc" },
-  { label: "Price - Asc", value: "price-asc" },
-  { label: "Price - Desc", value: "price-desc" }
-];
+import { useEffect, useState } from "react";
+import Filter from "@/components/partials/Filter";
+import GameTitleCard from "@/components/GameItemCard";
+import { GameTitle } from "@/types";
 
 const Wishlist = () => {
+  const [games, setGames] = useState<GameTitle[]>([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await fetch("/data.json");
+      const jsonData = await data.json();
+
+      setGames(jsonData);
+    };
+    getData();
+  }, []);
+
   return (
-    <div className="space-y-8 py-10">
-      <h1 className="text-3xl font-bold">Your Wishlist</h1>
-      <Select
-        disableAnimation={true}
-        label="Sort By"
-        size="sm"
-        placeholder="Select Option"
-        className="max-w-xs"
-        items={SORT_OPTIONS}
-      >
-        {(option) => <SelectItem key={option.value}>{option.label}</SelectItem>}
-      </Select>
+    <div className="space-y-4 pt-6 pb-4">
+      <h1 className="text-2xl font-bold">Your Wishlist</h1>
+      <Filter
+        label="Sort"
+        options={[
+          { label: "Title - Asc", value: "title-asc" },
+          { label: "Title - Desc", value: "title-desc" },
+          { label: "Price - Asc", value: "price-asc" },
+          { label: "Price - Desc", value: "price-desc" }
+        ]}
+      />
       <article className="w-full min-h-96 rounded-lg pb-8">
-        <ul className="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-5">
-          {games.map((game) => (
+        <ul className="grid grid-cols-2 xs:grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 gap-6">
+          {games!.map((game) => (
             <li className="mx-auto mb-2" key={game.title}>
               <GameTitleCard game={game} />
             </li>
