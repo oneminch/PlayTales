d68b36f8f9470fd7937d66c34ee030186a7d76c1
oneminch <27751688+oneminch@users.noreply.github.com>
2024-02-08 20:11:32 -0500

    refactor: make content more readable and styles more consistent

--- frontend/src/pages/ToS.tsx
+++ frontend/src/pages/ToS.tsx
@@ -1,14 +1,24 @@
+import Hero from "@/components/partials/Hero";
+
 const ToS = () => {
   return (
-    <>
-      <h1 className="text-3xl font-bold">Terms of Service</h1>
-      <p>
-        By signing up or creating an account, you understand that PlayTales is
-        not a real service and therefore shouldn't even have a Terms of Service
-        page. Your account will be permanently purged from the database after a
-        certain period of time.
-      </p>
-    </>
+    <div className="space-y-4 pt-6 pb-4 text-center">
+      <h1 className="text-2xl font-bold">Terms of Service</h1>
+
+      <article className="w-full md:w-2/3 mx-auto rounded-lg pb-8">
+        <p>
+          By signing up or creating an account, you understand that PlayTales is
+          not a real service and therefore shouldn't even have a Terms of
+          Service page. Your account will be permanently purged from the
+          database after a certain period of time.
+        </p>
+      </article>
+      <Hero
+        className="py-6"
+        textLabel="Play Your Next Story."
+        actionLink={{ label: "Browse Games", url: "/" }}
+      />
+    </div>
   );
 };
 
