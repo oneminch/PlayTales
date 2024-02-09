629ff591f13dc1f91840a5ac9c461362a899a76a
oneminch <27751688+oneminch@users.noreply.github.com>
2024-02-08 20:16:38 -0500

    refactor: switch to using new local JSON data file and custom component for price text

--- frontend/src/pages/Game.tsx
+++ frontend/src/pages/Game.tsx
@@ -1,35 +1,31 @@
 import { useParams } from "react-router-dom";
-import { Button, CircularProgress, Image } from "@nextui-org/react";
+import { Button, Chip, CircularProgress, Image } from "@nextui-org/react";
 import { SwiperSlide } from "swiper/react";
 import { Icon } from "@iconify/react";
 import Carousel from "@/components/Carousel";
-import games from "@/data";
 import type { GameTitle } from "@/types";
+import { useEffect, useState } from "react";
+import GamePriceText from "@/components/GamePriceText";
 
 const Game = () => {
   const { gameId } = useParams();
-  const currentGame = games.find((game) => game.id === gameId) as GameTitle;
+  const [currentGame, setCurrentGame] = useState<GameTitle>();
 
-  let priceText = null;
+  useEffect(() => {
+    const getData = async () => {
+      const data = await fetch("/data.json");
+      const jsonData = await data.json();
 
-  if (currentGame?.discount > 0) {
-    priceText = (
-      <>
-        <span className="bg-amber-400 rounded-md text-xs py-0.5 px-1">
-          -{currentGame?.discount}%
-        </span>
-        <span className="text-gray-500">
-          <s>${currentGame?.price}</s>
-        </span>
-        <span className="font-medium text-gray-700">
-          ${((1 - currentGame?.discount / 100) * currentGame?.price).toFixed(2)}
-        </span>
-      </>
-    );
-  } else {
-    priceText = (
-      <span className="font-medium text-gray-700">${currentGame?.price}</span>
-    );
+      const currGame = jsonData.find(
+        (game: GameTitle) => game.id === gameId
+      ) as GameTitle;
+      setCurrentGame(currGame);
+    };
+    getData();
+  }, [gameId]);
+
+  if (!currentGame) {
+    return <p>Loading...</p>;
   }
 
   return (
@@ -37,13 +33,13 @@ const Game = () => {
       <div className="flex flex-col-reverse md:flex-row items-start gap-x-0 gap-y-4 md:gap-y-0 md:gap-x-4">
         <section className="w-full md:w-2/3 flex flex-col gap-y-4 border-4 border-gray-300 bg-white/25 rounded-xl">
           <Carousel>
-            {currentGame.screenshots.map((screenshot) => (
+            {currentGame!.screenshots.map((screenshot) => (
               <SwiperSlide
                 key={screenshot}
                 className="text-center flex justify-center items-center"
               >
                 <Image
-                  alt={`Poster Image for ${currentGame?.title}`}
+                  alt={`Poster Image for ${currentGame!.title}`}
                   src={screenshot}
                   className="w-full h-full rounded-none object-cover opacity-100"
                   removeWrapper={true}
@@ -54,17 +50,38 @@ const Game = () => {
         </section>
         <section className="w-full md:w-1/3 pt-4 pb-8 px-8 flex flex-col gap-y-12 border border-gray-200 bg-white rounded-xl">
           <div className="space-y-4">
-            <h1 className="text-3xl font-bold">{currentGame?.title}</h1>
-            <p className="flex items-center gap-x-2">
-              <span>
-                <Icon icon="simple-icons:steam" />
-              </span>
-              <span>
-                <Icon icon="simple-icons:windows" />
-              </span>
-              <span>95 - Metacritic</span>
-            </p>
-            <p className="w-full space-x-2">{priceText}</p>
+            <h1 className="text-3xl font-bold">{currentGame!.title}</h1>
+            <div className="flex items-center gap-x-2">
+              <Chip
+                className="px-2 rounded-md"
+                size="sm"
+                classNames={{
+                  content: "px-1.5"
+                }}
+                startContent={
+                  <Icon className="text-sm" icon="simple-icons:steam" />
+                }
+              >
+                Steam
+              </Chip>
+              <Chip
+                className="px-2 rounded-md"
+                size="sm"
+                classNames={{
+                  content: "px-1.5"
+                }}
+                startContent={
+                  <Icon className="text-sm" icon="simple-icons:windows" />
+                }
+              >
+                Windows
+              </Chip>
+            </div>
+            <p>95 - Metacritic</p>
+            <GamePriceText
+              price={currentGame!.price}
+              discount={currentGame!.discount}
+            />
           </div>
           <div className="space-y-4">
             <Button
@@ -86,7 +103,7 @@ const Game = () => {
       </div>
       <div className="flex flex-col md:flex-row items-start gap-x-0 gap-y-4 md:gap-y-0 md:gap-x-4 border border-gray-200 bg-white rounded-xl">
         <section className="w-full md:w-2/3 p-8 space-y-4">
-          <h2 className="text-xl font-bold">About {currentGame.title}</h2>
+          <h2 className="text-xl font-bold">About {currentGame!.title}</h2>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
             consequatur atque neque repellat quia vel aliquam officia minus
@@ -104,6 +121,7 @@ const Game = () => {
               { label: "AnotherCritic", value: 91 }
             ].map((rating) => (
               <CircularProgress
+                key={rating.label}
                 label={rating.label}
                 aria-label={rating.label}
                 size="lg"
