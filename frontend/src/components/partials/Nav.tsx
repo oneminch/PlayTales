24118ba931ad896cca2315a8fa57b11c82ba9487
oneminch <27751688+oneminch@users.noreply.github.com>
2024-02-10 17:54:47 -0500

    feat: add badges to wishlist and cart links; modify cart link to show relevant information

--- frontend/src/components/partials/Nav.tsx
+++ frontend/src/components/partials/Nav.tsx
@@ -1,17 +1,39 @@
+import { Badge, Link } from "@nextui-org/react";
 import IconLink from "../IconLink";
+import { Icon } from "@iconify/react";
 
 const Nav = () => {
   return (
     <nav className="flex items-center">
       <ul className="flex items-center justify-between space-x-2">
         <li>
-          <IconLink path="/wishlist" icon="heroicons:heart-20-solid" />
+          <IconLink path="/login" icon="heroicons:user-20-solid" />
         </li>
         <li>
-          <IconLink path="/cart" icon="heroicons:shopping-cart-20-solid" />
+          <Badge
+            classNames={{
+              badge: "text-xs"
+            }}
+            content="8"
+          >
+            <IconLink path="/wishlist" icon="heroicons:heart-20-solid" />
+          </Badge>
         </li>
         <li>
-          <IconLink path="/account" icon="heroicons:user-20-solid" />
+          <Badge
+            classNames={{
+              badge: "text-xs"
+            }}
+            content="3"
+          >
+            <Link
+              href="/cart"
+              className="h-10 px-4 space-x-4 flex items-center justify-center text-lg border border-gray-200 rounded-xl bg-white"
+            >
+              <Icon icon="heroicons:shopping-cart-20-solid" />
+              <span className="text-sm">$107.99</span>
+            </Link>
+          </Badge>
         </li>
       </ul>
     </nav>
