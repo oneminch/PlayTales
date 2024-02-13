87c5fd66b97bb23f602e3b9f0843bacce663510a
oneminch <27751688+oneminch@users.noreply.github.com>
2024-02-12 21:14:23 -0500

    feat: make component take classes to extend its styles

--- frontend/src/components/IconLink.tsx
+++ frontend/src/components/IconLink.tsx
@@ -1,15 +1,31 @@
 import { Icon } from "@iconify/react";
 import { Link } from "@nextui-org/react";
 
-const IconLink = ({ path, icon }: { path: string; icon: string }) => {
+const IconLink = ({
+  path,
+  icon,
+  label,
+  className
+}: {
+  path: string;
+  icon: string;
+  label: string;
+  className: string;
+}) => {
   return (
     <Link
+      aria-label={label}
+      title={label}
       href={path}
-      className="w-10 h-10 flex items-center justify-center text-lg border border-gray-200 rounded-xl bg-white"
+      className={`w-10 h-10 flex items-center justify-center text-lg border border-gray-200 rounded-xl bg-white ${className}`}
     >
       <Icon icon={icon} />
     </Link>
   );
+};
+
+IconLink.defaultProps = {
+  className: ""
 };
 
 export default IconLink;
