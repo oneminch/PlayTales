661c2247df4b49d2e9fea67a0dfa34f7f83a69a9
oneminch <27751688+oneminch@users.noreply.github.com>
2024-02-12 21:19:12 -0500

    refactor: replace old search component with new one and change icon links to add accessible labels

--- frontend/src/components/partials/Header.tsx
+++ frontend/src/components/partials/Header.tsx
@@ -1,12 +1,16 @@
 import Nav from "./Nav";
-import Search from "../Search";
+import SearchForm from "../forms/SearchForm";
 import IconLink from "../IconLink";
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-between">
-      <IconLink path="/" icon="material-symbols:stadia-controller" />
-      <Search />
+    <header className="grid grid-cols-3">
+      <IconLink
+        label="Homepage"
+        path="/"
+        icon="material-symbols:stadia-controller"
+      />
+      <SearchForm />
       <Nav />
     </header>
   );
