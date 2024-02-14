e10fcf1f36a84f9a2d496337f85b4fefdba6b4be
oneminch <27751688+oneminch@users.noreply.github.com>
2024-02-13 23:04:01 -0500

    feat: add reset filter functionality to filters

--- frontend/src/components/Filter.tsx
+++ frontend/src/components/Filter.tsx
@@ -1,4 +1,4 @@
-import { Select, SelectItem, Selection } from "@nextui-org/react";
+import { Button, Select, SelectItem, Selection } from "@nextui-org/react";
 import { FilterInterface } from "@/types";
 import { Icon } from "@iconify/react";
 import useQueryParams from "@/hooks/useQueryParams";
@@ -24,26 +24,43 @@ const Filter = ({ label, options }: FilterInterface) => {
     }
   };
 
+  const handleFilterReset = () => {
+    setSelectedValue(new Set([]));
+    setParams(null);
+  };
+
   return (
-    <Select
-      label={label}
-      size="sm"
-      placeholder="Select an option"
-      selectedKeys={selectedValue}
-      className="sm:max-w-xs"
-      classNames={{
-        trigger: "rounded-lg border border-gray-200 bg-white",
-        popoverContent: "rounded-lg"
-      }}
-      onSelectionChange={handleSelectionChange}
-      selectorIcon={<Icon icon="heroicons:chevron-up-down-20-solid" />}
-    >
-      {options.map((option: { label: string; value: string }) => (
-        <SelectItem key={option.value} value={option.value}>
-          {option.label}
-        </SelectItem>
-      ))}
-    </Select>
+    <div className="flex items-center w-full sm:max-w-xs rounded-xl border border-gray-200 bg-white">
+      <Select
+        label={label}
+        size="sm"
+        placeholder="Select an option"
+        selectedKeys={selectedValue}
+        classNames={{
+          trigger: "bg-white rounded-l-xl rounded-r-none pr-1",
+          popoverContent: "rounded-lg"
+        }}
+        onSelectionChange={handleSelectionChange}
+        selectorIcon={<Icon icon="heroicons:chevron-up-down-20-solid" />}
+      >
+        {options.map((option: { label: string; value: string }) => (
+          <SelectItem key={option.value} value={option.value}>
+            {option.label}
+          </SelectItem>
+        ))}
+      </Select>
+      <Button
+        isIconOnly
+        isDisabled={selectedValue.size < 1}
+        onPress={handleFilterReset}
+        size="sm"
+        title={`Reset ${label} Filter`}
+        aria-label={`Reset ${label} Filter`}
+        className="w-6 h-6 min-w-6 min-h-6 rounded-full mx-2 text-lg p-0"
+      >
+        <Icon icon="heroicons:x-mark-20-solid" />
+      </Button>
+    </div>
   );
 };
 
