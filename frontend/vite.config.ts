083de7a409900e332283a7775c5288ca9fb79b58
oneminch <27751688+oneminch@users.noreply.github.com>
2024-03-06 22:43:49 -0500

    chore: reconfigure TypeScript and Vite to optimize for production

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,12 +2,24 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
     }
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8888",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, "")
+      }
+    }
+  },
+  mode: "production",
+  build: {
+    sourcemap: false
   }
 });
