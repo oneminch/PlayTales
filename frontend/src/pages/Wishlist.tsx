c47e7c6cd52f83ae4b76931771bff4f33c08283c
oneminch <27751688+oneminch@users.noreply.github.com>
2024-03-09 22:43:20 -0500

    feat: reimplement wishlist page logic using user and wishlist contexts

--- frontend/src/pages/Wishlist.tsx
+++ frontend/src/pages/Wishlist.tsx
@@ -1,43 +1,56 @@
-import { useEffect, useState } from "react";
-import Filter from "@/components/Filter";
 import ListItem from "@/components/cards/ListItem";
-import { GameItem } from "@/types";
+import { useAuthContext } from "@/context/auth-context";
+import Hero from "@/components/partials/Hero";
+import { useWishlistContext } from "@/context/wishlist-context";
 
 const Wishlist = () => {
-  const [games, setGames] = useState<GameItem[]>([]);
+  const { wishlist } = useWishlistContext();
+  const { isLoggedIn } = useAuthContext();
 
-  useEffect(() => {
-    const getData = async () => {
-      const data = await fetch("/data.json");
-      const jsonData = await data.json();
-
-      setGames(jsonData);
-    };
-    getData();
-  }, []);
+  let pageContent = null;
+  if (!isLoggedIn) {
+    pageContent = (
+      <Hero
+        className="bg-primary border border-secondary pt-12 pb-8"
+        textLabel="Log In to Wishlist Games."
+        actionLink={{ label: "Log In", url: "/login" }}
+      />
+    );
+  } else {
+    if (wishlist && wishlist.length > 0) {
+      pageContent = (
+        <article className="w-full min-h-96 pb-8">
+          <ul className="grid grid-cols-2 xs:grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 gap-6">
+            {wishlist.map((product) => (
+              <li className="mx-auto mb-2" key={product.title}>
+                <ListItem product={product} />
+              </li>
+            ))}
+          </ul>
+        </article>
+      );
+    } else {
+      pageContent = (
+        <Hero
+          className="bg-primary border border-secondary pt-12 pb-8"
+          textLabel="Wishlist Games to Keep Track of them Here."
+          actionLink={{
+            label: "Browse",
+            url: "/"
+          }}
+        />
+      );
+    }
+  }
 
   return (
-    <div className="space-y-4 pt-6 pb-4">
+    <>
       <h1 className="text-2xl font-bold">Your Wishlist</h1>
-      <Filter
-        label="Sort"
-        options={[
-          { label: "Title: A-Z", value: "title-asc" },
-          { label: "Title: Z-A", value: "title-desc" },
-          { label: "Price: Low to High", value: "price-asc" },
-          { label: "Price: High to Low", value: "price-desc" }
-        ]}
-      />
-      <article className="w-full min-h-96 pb-8">
-        <ul className="grid grid-cols-2 xs:grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 gap-6">
-          {games!.map((game) => (
-            <li className="mx-auto mb-2" key={game.title}>
-              <ListItem game={game} />
-            </li>
-          ))}
-        </ul>
-      </article>
-    </div>
+      <p className="text-primary-foreground">
+        Keep Track of Tales You Want to Play.
+      </p>
+      {pageContent}
+    </>
   );
 };
 
