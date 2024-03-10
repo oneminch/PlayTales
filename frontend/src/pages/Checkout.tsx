1423be5e32f7d074585cc2b34e0694cb3197a23c
oneminch <27751688+oneminch@users.noreply.github.com>
2024-03-09 22:42:27 -0500

    feat: implement checkout logic using cart and user contexts

--- frontend/src/pages/Checkout.tsx
+++ frontend/src/pages/Checkout.tsx
@@ -1,4 +1,3 @@
-import { GameItem, CartPriceInterface } from "@/types";
 import { useEffect, useState } from "react";
 import {
   Button,
@@ -11,64 +10,74 @@ import {
   AccordionItem
 } from "@nextui-org/react";
 import { Icon } from "@iconify/react";
+import { useCartContext } from "@/context/cart-context";
+import { useUserContext, userOrderMutationKey } from "@/context/user-context";
 import PaymentForm from "@/components/forms/PaymentForm";
 import AddressForm from "@/components/forms/AddressForm";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useIsMutating } from "@tanstack/react-query";
 
 const PaymentOptionIcon = ({ icon }: { icon: string }) => {
   return (
-    <span className="w-12 h-8 p-1 text-xs border border-gray-200 rounded-lg inline-flex items-center justify-center">
+    <span className="w-12 h-8 p-1 text-xs border border-secondary rounded-lg inline-flex items-center justify-center">
       <Icon icon={icon} />
     </span>
   );
 };
 
 const Checkout = () => {
-  const [games, setGames] = useState<GameItem[]>([]);
-  const [cartPrice, setCartPrice] = useState<CartPriceInterface>();
+  const { submitOrder } = useUserContext();
+  const {
+    products: cartProducts,
+    count: cartCount,
+    totalPrice: cartPrice,
+    clearCart
+  } = useCartContext();
+  const isMutating = useIsMutating({ mutationKey: [userOrderMutationKey] });
+
+  const [addressSaved, setAddressSaved] = useState(false);
+  const [paymentSaved, setPaymentSaved] = useState(false);
+
+  const { state } = useLocation();
+  const navigate = useNavigate();
 
   useEffect(() => {
-    const getData = async () => {
-      const data = await fetch("/data.json");
-      const jsonData = await data.json();
+    if (!state?.fromCart || cartCount === 0) {
+      return navigate("/cart");
+    }
+  }, [cartCount, state]);
 
-      const someData: GameItem[] = jsonData.slice(0, 3);
-      setGames(someData);
-    };
-    getData();
-  }, []);
-
-  useEffect(() => {
-    setCartPrice(
-      games.reduce(
-        (acc: CartPriceInterface, item: GameItem) => {
-          acc.price += item.price;
-          acc.discount += item.discount;
-
-          return acc;
-        },
-        { price: 0, discount: 0 }
-      )
-    );
-  }, [games]);
+  const handleOrder = () => {
+    submitOrder(Object.keys(cartProducts), clearCart);
+  };
 
   return (
-    <div className="space-y-4 pt-6 pb-4">
+    <>
       <h1 className="text-3xl font-bold">Checkout</h1>
       <article className="w-full grid grid-cols-3 gap-x-4 grid-rows-[auto_1fr] grid-flow-dense pb-8">
-        <div className="bg-white border border-gray-200 rounded-xl p-8 w-full row-span-2 col-span-2 space-y-4">
+        <div className="bg-primary border border-secondary rounded-xl p-8 w-full row-span-2 col-span-2 space-y-4">
           <section className="space-y-4">
             <h3 className="text-xl font-bold">Billing Address</h3>
-            <AddressForm />
+            <AddressForm
+              addressSaved={addressSaved}
+              setAddressSaved={setAddressSaved}
+            />
           </section>
           <Divider />
           <section className="space-y-4">
             <h3 className="text-xl font-bold">Payment</h3>
             <Accordion
               className="p-0 shadow-none"
+              keepContentMounted={true}
               itemClasses={{
                 title: "text-base font-semibold",
                 content: "pb-6"
               }}
+              disabledKeys={
+                !addressSaved
+                  ? ["Credit Card", "PayPal", "Apple Pay", "Google Pay"]
+                  : []
+              }
             >
               <AccordionItem
                 startContent={<PaymentOptionIcon icon="logos:mastercard" />}
@@ -76,7 +85,10 @@ const Checkout = () => {
                 aria-label="Credit Card"
                 title="Credit Card"
               >
-                <PaymentForm />
+                <PaymentForm
+                  paymentSaved={paymentSaved}
+                  setPaymentSaved={setPaymentSaved}
+                />
               </AccordionItem>
               <AccordionItem
                 startContent={<PaymentOptionIcon icon="logos:paypal" />}
@@ -105,7 +117,7 @@ const Checkout = () => {
             </Accordion>
           </section>
         </div>
-        <aside className="sticky top-4 space-y-4 row-span-1 *:bg-white *:border *:border-gray-200 *:rounded-xl *:py-2 *:px-6">
+        <aside className="sticky top-4 space-y-4 row-span-1 *:bg-primary *:border *:border-secondary *:rounded-xl *:py-2 *:px-6">
           <Card className="shadow-none">
             <CardHeader className="px-0">
               <h3 className="text-xl font-semibold">Order Summary</h3>
@@ -114,37 +126,34 @@ const Checkout = () => {
             <CardBody className="px-0 space-y-4">
               <p className="flex items-center justify-between">
                 <span>Full Price:</span>
-                <span className="text-gray-500">
-                  <s>${cartPrice && cartPrice!.price}</s>
+                <span className="text-primary-foreground/85">
+                  <s>${cartPrice.totalOriginalPrice}</s>
                 </span>
               </p>
               <p className="flex items-center justify-between">
                 <span>Your Savings:</span>
                 <span className="text-emerald-500 font-semibold">
                   -$
-                  {cartPrice &&
-                    ((cartPrice!.discount / 100) * cartPrice!.price).toFixed(2)}
+                  {(
+                    cartPrice.totalOriginalPrice -
+                    cartPrice.totalDiscountedPrice
+                  ).toFixed(2)}
                 </span>
               </p>
               <Divider />
               <p className="flex items-center justify-between">
                 <span>Your Total:</span>
                 <span className="font-semibold">
-                  $
-                  {cartPrice &&
-                    (
-                      (1 - cartPrice!.discount / 100) *
-                      cartPrice!.price
-                    ).toFixed(2)}
+                  ${cartPrice.totalDiscountedPrice}
                 </span>
               </p>
             </CardBody>
             <CardFooter className="px-0">
               <Button
-                isDisabled
-                isLoading
+                isDisabled={!addressSaved || !paymentSaved || isMutating > 0}
+                onPress={handleOrder}
                 spinnerPlacement="end"
-                className="w-full font-medium bg-gray-900 text-gray-50 py-1 rounded-xl disabled:cursor-not-allowed"
+                className="w-full font-medium bg-foreground text-background py-1 rounded-xl disabled:cursor-not-allowed"
               >
                 Submit Order
               </Button>
@@ -152,7 +161,7 @@ const Checkout = () => {
           </Card>
         </aside>
       </article>
-    </div>
+    </>
   );
 };
 
