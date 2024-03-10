7dd3c56feb09d9754b954a7fcd8f1b06bea5f2fe
oneminch <27751688+oneminch@users.noreply.github.com>
2024-03-09 22:45:28 -0500

    refactor: implement data fetching for homepage and utilize hooks for additional logic

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,20 +1,51 @@
 import Banner from "@/components/partials/Banner";
 import Hero from "@/components/partials/Hero";
-import ItemList from "@/components/partials/ItemList";
+import ProductList from "@/components/partials/ProductList";
+import { useEffect, useState } from "react";
+import { useAuthContext } from "@/context/auth-context";
+import { useSearchParams } from "react-router-dom";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import SearchForm from "@/components/forms/SearchForm";
 
 const Home = () => {
+  const { isLoggedIn } = useAuthContext();
+  const [searchParams] = useSearchParams();
+  const [queryParams, setQueryParams] = useState("");
+
+  useEffect(() => {
+    if (searchParams.toString().trim()) {
+      setQueryParams(`?${searchParams.toString()}`);
+    } else {
+      setQueryParams("");
+    }
+  }, [searchParams]);
+
+  const queryClient = useQueryClient();
+
+  const { isError, isFetching, data } = useQuery<any>(
+    {
+      queryKey: [`/products${queryParams}`],
+      placeholderData: { products: [], count: 0 }
+    },
+    queryClient
+  );
+
   return (
     <>
+      <SearchForm className="block md:hidden" />
+
       <Hero
-        className="bg-white border border-gray-200 pt-10 pb-6"
-        textLabel="Play Your Next Story."
-        actionLink={{ label: "Browse", url: "#browse" }}
+        className="text-foreground bg-primary border border-secondary pt-12 pb-8"
+        textLabel="Play Your Next Tale."
       />
-      <ItemList />
+
+      <ProductList data={data} isError={isError} isLoading={isFetching} />
+
       <Banner
-        className="bg-white border border-gray-200"
+        className="text-foreground bg-primary border border-secondary"
         textLabel="Become a Member & Save More."
         actionLink={{ label: "Join Now", url: "/signup" }}
+        isVisible={!isLoggedIn}
       />
     </>
   );
