16215bbbdb992ba6dca7ad7d59eaa56bcb25468c
oneminch <27751688+oneminch@users.noreply.github.com>
2024-03-09 22:36:56 -0500

    feat: implement a working search page using useQuery

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -1,7 +1,39 @@
-import ItemList from "@/components/partials/ItemList";
+import ProductList from "@/components/partials/ProductList";
+import DefaultPageLayout from "@/layout/DefaultPageLayout";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+import { useParams, useSearchParams } from "react-router-dom";
 
 const Search = () => {
-  return <ItemList />;
+  const [searchParams] = useSearchParams();
+  const { searchQuery } = useParams();
+  const [queryParams, setQueryParams] = useState("");
+
+  useEffect(() => {
+    if (searchParams.toString().trim()) {
+      setQueryParams(`?${searchParams.toString()}`);
+    } else {
+      setQueryParams("");
+    }
+  }, [searchParams]);
+
+  const queryClient = useQueryClient();
+
+  const { isFetching, isError, data } = useQuery<any>(
+    {
+      queryKey: [`/search/${searchQuery}${queryParams}`],
+      placeholderData: { products: [], count: 0 },
+      enabled: !!searchQuery
+    },
+    queryClient
+  );
+
+  return (
+    <DefaultPageLayout title={`"${searchQuery}"`}>
+      <h1 className="text-2xl font-bold">Search Results for "{searchQuery}"</h1>
+      <ProductList data={data} isError={isError} isLoading={isFetching} />
+    </DefaultPageLayout>
+  );
 };
 
 export default Search;
