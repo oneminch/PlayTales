b2158520bf07e71c624d78e1b2753a1fb2789df8
oneminch <27751688+oneminch@users.noreply.github.com>
2024-03-09 22:17:08 -0500

    feat: implement search suggestion functionality

--- frontend/src/components/forms/SearchForm.tsx
+++ frontend/src/components/forms/SearchForm.tsx
@@ -1,50 +1,76 @@
-import useFetch from "@/hooks/useFetch";
-import { GameItem } from "@/types";
+import type { Product } from "@/types";
 import { Icon } from "@iconify/react";
 import {
-  Button,
   Input,
   Listbox,
   ListboxItem,
-  ListboxSection
+  ListboxSection,
+  cn
 } from "@nextui-org/react";
 import SearchItem from "../cards/SearchItem";
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 
-const SearchForm = () => {
-  const { data: games } = useFetch("/data.json", []);
+const SearchForm = ({ className }: { className: string }) => {
   const [searchQuery, setSearchQuery] = useState("");
+  const [searchUrl, setSearchUrl] = useState("");
+  const [isSearching, setIsSearching] = useState(false);
+
+  const queryClient = useQueryClient();
+
+  const { isFetching, isError, data } = useQuery<any>(
+    {
+      queryKey: [searchUrl],
+      placeholderData: { products: [], count: 0 },
+      enabled: !!searchUrl
+    },
+    queryClient
+  );
 
   const navigate = useNavigate();
 
+  useEffect(() => {
+    if (searchQuery && searchQuery.trim().length > 1) {
+      setSearchUrl(`/search/${encodeURIComponent(searchQuery)}`);
+      setIsSearching(true);
+    } else {
+      setSearchUrl("");
+      setIsSearching(false);
+    }
+  }, [searchQuery]);
+
   const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchQuery(event.target.value);
-    // Handle suggestions here
   };
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-    if (searchQuery && searchQuery.trim().length > 0) {
-      navigate(`/search?q=${encodeURIComponent(searchQuery)}`);
+    if (isSearching) {
+      navigate(searchUrl);
+      afterSearch();
     }
   };
 
+  const afterSearch = () => {
+    setSearchQuery("");
+    setIsSearching(false);
+  };
+
   return (
-    <section className="w-full relative group">
+    <section className={cn("w-full relative group", className)}>
       <form
         onSubmit={handleSubmit}
         className="w-full h-10 flex items-center text-lg"
       >
         <Input
           type="text"
-          size="sm"
           radius="md"
           placeholder="Search games..."
-          className="w-full bg-white rounded-xl"
+          className="w-full bg-primary rounded-xl"
           classNames={{
-            input: "px-2",
-            base: "h-full p-0 border border-gray-200 bg-white ",
+            input: "px-2 text-base",
+            base: "h-full p-0 border border-secondary bg-primary",
             mainWrapper: "p-0",
             inputWrapper:
               "h-full p-1 bg-transparent shadow-none focus-within:!bg-transparent",
@@ -52,7 +78,7 @@ const SearchForm = () => {
           }}
           startContent={
             <Icon
-              className="ml-2 text-gray-500"
+              className="ml-2 text-primary-foreground"
               icon="heroicons:magnifying-glass-20-solid"
             />
           }
@@ -60,42 +86,71 @@ const SearchForm = () => {
           onChange={handleInput}
         />
       </form>
-      <div
-        className={`w-full rounded-xl z-50 absolute top-full mt-1 shadow-lg left-0 bg-white border border-gray-200 transition-all duration-100 group-focus-within:translate-y-0 group-focus-within:opacity-100 group-focus-within:visible translate-y-2 opacity-0 invisible`}
-      >
-        <Listbox
-          shouldFocusWrap={true}
-          aria-label="Search Suggestions"
-          className="p-2"
+      {isSearching && (
+        <div
+          className={`w-full rounded-xl z-50 absolute top-full mt-1 shadow-lg left-0 bg-primary border border-secondary transition-all duration-100 group-focus-within:translate-y-0 group-focus-within:opacity-100 group-focus-within:visible translate-y-2 opacity-0 invisible`}
         >
-          <ListboxSection showDivider>
-            {games &&
-              games.slice(0, 3).map((game: GameItem) => (
-                <ListboxItem
-                  key={game.id}
-                  textValue={game.title}
-                  className="*:h-16 rounded-lg"
-                  href={`/games/${game.id}`}
-                >
-                  <SearchItem game={game} />
+          <Listbox
+            shouldFocusWrap={true}
+            aria-label="Search Suggestions"
+            className="py-2 px-4"
+            onAction={() => {
+              afterSearch();
+            }}
+            disabledKeys={["loading", "error", "empty"]}
+          >
+            <ListboxSection showDivider={data && data.count > 0}>
+              {isFetching && (
+                <ListboxItem key="loading" className="rounded-lg text-center">
+                  Loading Suggestions
                 </ListboxItem>
-              ))}
-          </ListboxSection>
-          <ListboxSection>
-            <ListboxItem
-              className="rounded-lg"
-              key="show-all"
-              textValue="Show All Results"
+              )}
+              {isError && (
+                <ListboxItem key="error" className="rounded-lg text-center">
+                  Error Loading Suggestions
+                </ListboxItem>
+              )}
+              {data.count === 0 && (
+                <ListboxItem key="empty" className="rounded-lg text-center">
+                  No Suggestions Found.
+                </ListboxItem>
+              )}
+              {data.products &&
+                data.products.slice(0, 2).map((suggestion: Product) => (
+                  <ListboxItem
+                    key={suggestion.id}
+                    textValue={suggestion.title}
+                    className="rounded-lg"
+                    href={`/products/${suggestion.id}`}
+                  >
+                    <SearchItem product={suggestion} />
+                  </ListboxItem>
+                ))}
+            </ListboxSection>
+            <ListboxSection
+              className={cn(
+                "w-full",
+                isSearching && data.count > 0 ? "block" : "hidden"
+              )}
             >
-              <Button className="w-full bg-gray-900 text-gray-50">
+              <ListboxItem
+                className="text-center py-2 w-full h-full rounded-lg bg-foreground text-background"
+                key="show-all-results"
+                textValue="Show All Results"
+                href={searchUrl && searchUrl}
+              >
                 Show All Results
-              </Button>
-            </ListboxItem>
-          </ListboxSection>
-        </Listbox>
-      </div>
+              </ListboxItem>
+            </ListboxSection>
+          </Listbox>
+        </div>
+      )}
     </section>
   );
+};
+
+SearchForm.defaultProps = {
+  className: ""
 };
 
 export default SearchForm;
