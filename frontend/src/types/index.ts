42271970ad93c5c8acd04fc48f561cc7d6e31fed
oneminch <27751688+oneminch@users.noreply.github.com>
2024-03-09 22:01:01 -0500

    refactor: create and export more custom types for development needs

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,20 +1,93 @@
-interface GameItem {
-  id: string;
-  title: string;
-  posterImage: string;
-  discount: number;
-  price: number;
-  screenshots: string[];
+import { ReactNode } from "react";
+
+interface ChildrenNodes {
+  children: React.ReactNode;
 }
 
-interface FilterInterface {
+interface Product {
+  id: string;
+  title: string;
+  description: string;
+  discount: number;
+  price: number;
+  ratings: number[];
+  genre: string[];
+  poster: string;
+  screenshots: string[];
+  releaseDate: string;
+  platform: string;
+  developer: string;
+  publisher: string;
+}
+
+interface FilterOptions {
   label: string;
   options: { label: string; value: string }[];
 }
 
-interface CartPriceInterface {
+interface ProductPrice {
   price: number;
   discount: number;
 }
 
-export type { CartPriceInterface, FilterInterface, GameItem };
+interface LayoutProps {
+  title: string;
+  description?: string;
+  className?: string;
+  children: ReactNode;
+}
+
+interface CartPrice {
+  totalOriginalPrice: number;
+  totalDiscountedPrice: number;
+}
+
+interface AuthContext {
+  isLoggedIn: boolean;
+  logIn: () => void;
+  logOut: () => void;
+}
+
+interface CartContext {
+  products: Record<string, Product>;
+  count: number;
+  totalPrice: CartPrice;
+  toggleProductCartStatus: (product: Product) => void;
+  getProductCartStatus: (productId: string) => boolean;
+  clearCart: () => void;
+}
+
+interface ThemeContext {
+  theme: string;
+  toggleTheme: () => void;
+}
+
+interface UserContext {
+  userInfo: Record<string, any> | null;
+  userOrders: Record<string, any> | null;
+  submitOrder: (productIds: string[], callback: () => any) => void;
+}
+
+interface WishlistContext {
+  wishlist: Product[];
+  count: number;
+  getProductWishlistStatus: (productId: string) => {
+    isProductWishlisted: boolean;
+    wishlistAction: string;
+  };
+  toggleProductWishlistStatus: (productId: string) => void;
+}
+
+export type {
+  AuthContext,
+  CartContext,
+  CartPrice,
+  ChildrenNodes,
+  FilterOptions,
+  LayoutProps,
+  Product,
+  ProductPrice,
+  ThemeContext,
+  UserContext,
+  WishlistContext
+};
