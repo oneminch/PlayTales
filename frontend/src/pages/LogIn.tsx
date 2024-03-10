eb4a8ed7b57a14bf81a7be91d26a236e87255ff0
oneminch <27751688+oneminch@users.noreply.github.com>
2024-03-09 22:39:08 -0500

    feat: add proper redirects for auth pages depending on auth status

--- frontend/src/pages/LogIn.tsx
+++ frontend/src/pages/LogIn.tsx
@@ -2,39 +2,50 @@ import { Button } from "@nextui-org/react";
 import { Icon } from "@iconify/react";
 import Hero from "@/components/partials/Hero";
 import LogInForm from "@/components/forms/LogInForm";
+import { useEffect } from "react";
+import { useAuthContext } from "@/context/auth-context";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const LogIn = () => {
+  const { isLoggedIn } = useAuthContext();
+  const navigate = useNavigate();
+  const { state } = useLocation();
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate(state?.redirectTo || "/");
+    }
+  }, [navigate, isLoggedIn]);
+
   return (
-    <div className=" bg-white border border-gray-200 rounded-xl py-4 md:py-10">
-      <article className="w-full lg:w-4/5 mx-auto flex flex-col md:flex-row items-center justify-center gap-y-4 lg:gap-y-0 lg:gap-x-4">
-        <section className="w-full md:w-1/2 space-y-4 mx-auto py-6 px-10 md:py-0 border-b border-b-gray-200/50 md:border-b-0 md:border-r md:border-r-gray-200/50">
-          <h1 className="text-3xl font-bold">Log In</h1>
-          <LogInForm />
-          <p>Or sign in with</p>
-          <div className="flex items-center gap-2 *:w-full">
-            <Button
-              startContent={
-                <Icon className="shrink-0" icon="simple-icons:google" />
-              }
-            >
-              Google
-            </Button>
-            <Button
-              startContent={
-                <Icon className="shrink-0" icon="simple-icons:facebook" />
-              }
-            >
-              Facebook
-            </Button>
-          </div>
-        </section>
-        <Hero
-          className="p-8 md:p-2 md:w-1/2"
-          textLabel="Don't Have an Account?"
-          actionLink={{ label: "Sign Up", url: "/signup" }}
-        />
-      </article>
-    </div>
+    <article className="w-full lg:w-4/5 mx-auto flex flex-col md:flex-row items-center justify-center gap-y-4 lg:gap-y-0 lg:gap-x-4">
+      <section className="w-full md:w-1/2 space-y-4 mx-auto py-4 px-6 md:py-0 md:px-10 border-b border-b-secondary/50 md:border-b-0 md:border-r md:border-r-secondary/50">
+        <h1 className="text-3xl font-bold">Log In</h1>
+        <LogInForm />
+        <p>Or sign in with</p>
+        <div className="flex items-center gap-2 *:w-full">
+          <Button
+            className="text-foreground bg-background border border-secondary rounded-xl"
+            startContent={
+              <Icon className="shrink-0" icon="logos:google-icon" />
+            }
+          >
+            Google
+          </Button>
+          <Button
+            className="text-foreground bg-background border border-secondary rounded-xl"
+            startContent={<Icon className="shrink-0" icon="logos:facebook" />}
+          >
+            Facebook
+          </Button>
+        </div>
+      </section>
+      <Hero
+        className="p-8 md:p-2 md:w-1/2"
+        textLabel="Don't Have an Account?"
+        actionLink={{ label: "Sign Up", url: "/signup" }}
+      />
+    </article>
   );
 };
 
