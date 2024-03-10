a335c0668cdc223e5cb92511dbf79d2ffe1738f3
oneminch <27751688+oneminch@users.noreply.github.com>
2024-03-09 21:34:43 -0500

    feat: add a generic component for rendering loading and error messages

---
+++ frontend/src/components/Placeholder.tsx
@@ -0,0 +1,49 @@
+import { Icon } from "@iconify/react";
+import { Link, cn } from "@nextui-org/react";
+
+const Placeholder = ({
+  icon,
+  primaryText,
+  showIf,
+  secondaryText,
+  actionLink,
+  className
+}: {
+  icon?: string;
+  primaryText: string;
+  showIf: boolean;
+  secondaryText?: string;
+  actionLink?: Record<string, string>;
+  className: string;
+}) => {
+  return (
+    showIf && (
+      <div
+        className={cn(
+          "flex flex-col items-center justify-center gap-y-4 rounded-xl p-6 bg-primary text-primary-foreground border border-secondary min-h-56",
+          className
+        )}
+      >
+        {icon && <Icon className="text-5xl" icon={icon} />}
+        <h3 className="text-lg sm:text-xl font-semibold text-center">
+          {primaryText}
+        </h3>
+        {secondaryText && <p className="text-center">{secondaryText}</p>}
+        {actionLink && (
+          <Link
+            href={actionLink.url}
+            className="px-4 py-1.5 rounded-xl font-medium text-foreground bg-background"
+          >
+            {actionLink.label}
+          </Link>
+        )}
+      </div>
+    )
+  );
+};
+
+Placeholder.defaultProps = {
+  className: ""
+};
+
+export default Placeholder;
